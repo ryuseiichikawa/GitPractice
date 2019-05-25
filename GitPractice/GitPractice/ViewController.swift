c1f48552ede0e941bd07693498174ff8093e5e1f
ryuseiichikawa <02262ne1@gmail.com>
2019-05-26 00:14:56 +0900

    initviewメソッド追加

--- GitPractice/GitPractice/ViewController.swift
+++ GitPractice/GitPractice/ViewController.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
-import WebKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initview()
     }
 
+    func initview() {
+        
+    }
 
 }
 
