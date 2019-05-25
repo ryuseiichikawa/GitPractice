23f6e28e626942cae42880038c166550bf484766
ryuseiichikawa <02262ne1@gmail.com>
2019-05-26 00:15:58 +0900

    市川メソッド追加

--- GitPractice/GitPractice/ViewController.swift
+++ GitPractice/GitPractice/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func ichikawa() {
+        
+    }
 
 
 }
