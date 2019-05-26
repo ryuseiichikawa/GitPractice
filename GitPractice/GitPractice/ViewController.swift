7c0367b130f07603264d56b6aac77dabd5218a55
ryuseiichikawa <02262ne1@gmail.com>
2019-05-26 10:30:39 +0900

    ViewController書き換え

--- GitPractice/GitPractice/ViewController.swift
+++ GitPractice/GitPractice/ViewController.swift
@@ -25,3 +25,14 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
